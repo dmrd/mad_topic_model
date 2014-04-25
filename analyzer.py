ef71ecf21fb368408442067f45dd321542bdbf69
Charles Marsh <crmarsh@princeton.edu>
2014-04-25 15:45:04 -0400

    More text analysis functions

--- analyzer.py
+++ analyzer.py
@@ -2,9 +2,79 @@ from nltk.corpus import cmudict
 from nltk import pos_tag, word_tokenize, sent_tokenize
 
 
-def tag_text(text):
+PUNCTUATION_TAGS = ['.', ':', ',']
+
+
+def tag_sentences(text):
+    """
+    Splits the text into sentences and then tokenizes them individually.
+    Returns a list of lists of (word, tag) pairs, where each sublist
+    represents a sentence.
+    """
     sentences = sent_tokenize(text)
     return map(lambda s: pos_tag(word_tokenize(s)), sentences)
+
+
+def tag_text(text):
+    """Tags a body of text, returning a list of (word, tag) pairs."""
+    return sum(tag_sentences(text), [])
+
+
+def syllabic_representation(text):
+    """
+    Maps text to a list of syllables and puncutation marks, e.g.:
+    >>> syllabic_representation("Okay; I am ready.")
+    >>> [2, ';', 1, 1, 2, '.']
+    """
+    def to_syllables((word, tag)):
+        if tag in PUNCTUATION_TAGS:
+            return (word, tag)
+        return num_syllables(word)
+    return map(to_syllables, tag_text(text))
+
+
+def syllable_counts(text):
+    """
+    Returns a list of (syllable_counts, punctuation) pairs, where
+    syllable_counts is a list of integers representing the number of
+    syllables in the words preceding the punctuation. If the text ends without
+    a piece of punctuation, the final punctuation will be None. E.g.:
+    >>> syllable_counts("Okay; I am ready.")
+    >>> [([2], ';'), ([1, 1, 2], '.')]
+    """
+    result = []
+    temp = []
+    for x in syllabic_representation(text):
+        if type(x) is int:
+            temp.append(x)
+        else:
+            result.append((temp, x))
+            temp = []
+    if temp:
+        result.append((temp, None))
+    return result
+
+
+def word_counts(text):
+    """
+    Returns a list of (word_counts, punctuation) pairs, where word_counts
+    is the number of words preceding the punctuation. If the text ends without
+    a piece of punctuation, the final punctuation will be None. E.g.:
+    >>> word_counts("Okay; I am ready.")
+    >>> [(1, ';'), (3, '.')]
+    """
+    # Could use syllable_counts, return length of lists; faster to do manually
+    result = []
+    counter = 0
+    for (word, tag) in tag_text(text):
+        if tag in PUNCTUATION_TAGS:
+            result.append((counter, word))
+            counter = 0
+        else:
+            counter += 1
+    if counter:
+        result.append((counter, None))
+    return result
 
 
 def num_syllables(word, UNIQUE=True):
