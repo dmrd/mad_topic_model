5dba4488ad4b72a691b832968879d5ddb4a0346d
Charles Marsh <crmarsh@princeton.edu>
2014-04-30 21:51:57 -0400

    n-grams for syllabic features

--- features/analyzer.py
+++ features/analyzer.py
@@ -1,9 +1,35 @@
 from nltk.corpus import cmudict
+from nltk.util import ngrams
 from nltk import pos_tag, word_tokenize, sent_tokenize
 
 
 PUNCTUATION_TAGS = ['.', ':', ',']
 d = cmudict.dict()
+
+
+def syllable_ngrams(text, n):
+    """
+    Returns the n-grams of syllable usage by breaking each word down into
+    (# syllables) and then taking n-grams on that sequence. In this way,
+    it essentially ignores punctuation. E.g.:
+    >>> syllable_ngrams("It is: overwhelming.", 2)
+    >>> [(1, 1), (1, 4)]
+    """
+    syllables = [s for (s, _) in syllable_counts(text)]
+    syllables = sum(syllables, [])
+    return ngrams(syllables, n)
+
+
+def syllable_count_ngrams(text, n):
+    """
+    Returns the n-grams of syllable counts between punctuation. That is,
+    it sums (# syllables) for all the words between punctuation marks and
+    returns the n-grams on that sequence. E.g.:
+    >>> syllable_count_ngrams("It is: overwhelming. We should go.", 2)
+    >>> [(2, 4), (4, 3)]
+    """
+    syllables = [s for (s, _) in syllable_counts(text, TOTAL=True)]
+    return ngrams(syllables, n)
 
 
 def tag_sentences(text):
@@ -34,23 +60,35 @@ def syllabic_representation(text):
     return map(to_syllables, tag_text(text))
 
 
-def syllable_counts(text):
+def syllable_counts(text, TOTAL=False):
     """
     Returns a list of (syllable_counts, punctuation) pairs, where
     syllable_counts is a list of integers representing the number of
-    syllables in the words preceding the punctuation. If the text ends without
-    a piece of punctuation, the final punctuation will be None. E.g.:
+    syllables in the words preceding the punctuation. If TOTAL is True,
+    then the lists of syllables are summed. If the text ends without
+    a piece of punctuation, the final punctuation will be None.
+    E.g.:
     >>> syllable_counts("Okay; I am ready.")
     >>> [([2], ';'), ([1, 1, 2], '.')]
+    >>> syllable_counts("Okay; I am ready.", TOTAL=True)
+    >>> [(2, ';'), (4, '.')]
     """
     result = []
-    temp = []
+
+    def reset():
+        if TOTAL:
+            return 0
+        return []
+    temp = reset()
+
     for x in syllabic_representation(text):
         if type(x) is int:
-            temp.append(x)
+            if not TOTAL:
+                x = [x]
+            temp += x
         else:
             result.append((temp, x))
-            temp = []
+            temp = reset()
     if temp:
         result.append((temp, None))
     return result
@@ -78,15 +116,16 @@ def word_counts(text):
     return result
 
 
-def stress_counts(text, SYLLABLES=True):
+def stress_counts_by_phone(text, PHONES=True):
     """
-    Returns a list of (stress_counts, punctuation) pairs, where stress_counts
+    Returns a list of (stress_counts, punctuation) pairs, where stress
+    is evaluated on a per-phone basis, and stress_counts
     is a list of integers and None values preceding the punctuation. If the
     text ends without punctuation, the final punctuation value will be None.
     E.g.:
-    >>> stress_counts("Okay; I am ready.")
+    >>> stress_counts_by_phone("Okay; I am ready.")
     >>> [([('OW', 2), ('K', 0), ('EY', 1)], ';'), ([('AY', 1), ('AE', 1), ('M', 0), ('R', 0), ('EH', 1), ('D', 0), ('IY', -1)], '.')]
-    >>> stress_counts("Okay; I am ready.", SYLLABLES=False)
+    >>> stress_counts_by_phone("Okay; I am ready.", SYLLABLES=False)
     >>> [([2, 0, 1], ';'), ([1, 1, 0, 0, 1, 0, -1], '.')]
     """
     result = []
@@ -96,10 +135,24 @@ def stress_counts(text, SYLLABLES=True):
             result.append((temp, word))
             temp = []
         else:
-            temp += stress(word, SYLLABLES=SYLLABLES)
+            temp += stress(word, PHONES=PHONES)
     if temp:
         result.append((temp, None))
     return result
+
+
+def stress_counts_by_syllable(text):
+    """
+    Returns a list of (stress_counts, punctuation) pairs, where stress
+    is evaluated on a per-syllable basis, and stress_counts
+    is a list of integers and None values preceding the punctuation. If the
+    text ends without punctuation, the final punctuation value will be None.
+    E.g.:
+    >>> stress_counts_by_syllable("Okay; I am ready.")
+    >>> [([2, 1], ';'), ([1, 1, 1, -1], '.')]
+    """
+    counts = stress_counts_by_phone(text, PHONES=False)
+    return [(filter(lambda x: x != 0, run), punc) for (run, punc) in counts]
 
 
 def num_syllables(word, UNIQUE=True):
@@ -122,10 +175,10 @@ def num_syllables(word, UNIQUE=True):
         return nsyls
 
 
-def stress(word, SYLLABLES=True):
+def stress(word, PHONES=True):
     """
-    Maps a word to its syllables and their stresses. (1) indicates primary,
-    (2) secondary, and (-1) neutral, and (0) no stress. If SYLLABLES is False,
+    Maps a word to its phones and their stresses. (1) indicates primary,
+    (2) secondary, and (-1) neutral, and (0) no stress. If PHONES is False,
     only the stress information is returned. E.g.:
     >>> stress("fire")
     >>> [('F', 0), ('AY', 1), ('ER', -1)]
@@ -142,6 +195,6 @@ def stress(word, SYLLABLES=True):
         return (s, 0)
     syllables = d[word.lower()][0]
     stresses = map(extract_stress, syllables)
-    if not SYLLABLES:
+    if not PHONES:
         return map(lambda x: x[1], stresses)
     return stresses
--- test.py
+++ test.py
@@ -16,22 +16,39 @@ class AnalyzerTests(unittest.TestCase):
         self.assertEqual(analyzer.stress("fire"),
                          [('F', 0), ('AY', 1), ('ER', -1)])
         self.assertEqual(
-            analyzer.stress("fire", SYLLABLES=False), [0, 1, -1])
+            analyzer.stress("fire", PHONES=False), [0, 1, -1])
 
     def test_syllable_counts(self):
         self.assertEqual(analyzer.syllable_counts("Okay; I am ready."),
                          [([2], ';'), ([1, 1, 2], '.')])
+        self.assertEqual(
+            analyzer.syllable_counts("Okay; I am ready.", TOTAL=True),
+            [(2, ';'), (4, '.')])
 
     def test_word_counts(self):
         self.assertEqual(analyzer.word_counts("Okay; I am ready. Go"),
                          [(1, ';'), (3, '.'), (1, None)])
 
-    def test_stress_counts(self):
-        self.assertEqual(analyzer.stress_counts("Okay; I am ready."),
+    def test_stress_counts_by_phone(self):
+        self.assertEqual(analyzer.stress_counts_by_phone("Okay; I am ready."),
                          [([('OW', 2), ('K', 0), ('EY', 1)], ';'), ([('AY', 1), ('AE', 1), ('M', 0), ('R', 0), ('EH', 1), ('D', 0), ('IY', -1)], '.')])
         self.assertEqual(
-            analyzer.stress_counts("Okay; I am ready.", SYLLABLES=False),
+            analyzer.stress_counts_by_phone(
+                "Okay; I am ready.", PHONES=False),
             [([2, 0, 1], ';'), ([1, 1, 0, 0, 1, 0, -1], '.')])
+
+    def test_stress_counts_by_syllable(self):
+        self.assertEqual(
+            analyzer.stress_counts_by_syllable("Okay; I am ready."),
+            [([2, 1], ';'), ([1, 1, 1, -1], '.')])
+
+    def test_ngrams(self):
+        self.assertEqual(
+            analyzer.syllable_ngrams("It is: overwhelming.", 2), [(1, 1), (1, 4)])
+        self.assertEqual(
+            analyzer.syllable_count_ngrams(
+                "It is: overwhelming. We should go.", 2),
+            [(2, 4), (4, 3)])
 
 
 if __name__ == "__main__":
