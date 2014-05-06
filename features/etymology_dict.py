c43d4c7ac27cf70c5384502d31998f70596c0bf8
Charles Marsh <crmarsh@princeton.edu>
2014-05-06 16:38:49 -0400

    ngrams for etymology

--- features/etymology_dict.py
+++ features/etymology_dict.py
@@ -2,33 +2,30 @@ from nltk import PorterStemmer, WordNetLemmatizer
 from Levenshtein import ratio
 
 
-def etydict():
+class EtymologyDict(object):
     d = {}
-    complete_dict = file("origin/output.txt").readlines()
-    for line in complete_dict:
-        split = line.split(',')
-        d[split[0].strip()] = split[1].strip()
-    return d
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
 
+    def load(self):
+        complete_dict = file("origin/output.txt").readlines()
+        for line in complete_dict:
+            split = line.split(',')
+            self.d[split[0].strip()] = split[1].strip()
 
-d = etydict()
-stemmer = PorterStemmer()
-lemmatizer = WordNetLemmatizer()
+    def lookup(self, s):
+        if not self.d:
+            self.load()
 
-
-def lookup(s):
-    try:
-        return d[s.lower()]
-    except KeyError:
         try:
-            print "Stemming: " + stemmer.stem(s)
-            return d[stemmer.stem(s).lower()]
+            return self.d[s.lower()]
         except KeyError:
             try:
-                print "Lemmatizing: " + lemmatizer.lemmatize(s)
-                return d[lemmatizer.lemmatize(s).lower()]
+                return self.d[self.stemmer.stem(s).lower()]
             except KeyError:
-                (score, match) = max((ratio(s, t), t) for t in d)
-                print "Levenshtein: " + match
-                d[s] = d[match]
-                return d[match]
+                try:
+                    return self.d[self.lemmatizer.lemmatize(s).lower()]
+                except KeyError:
+                    (score, match) = max((ratio(s, t), t) for t in self.d)
+                    self.d[s] = self.d[match]
+                    return self.d[match]
