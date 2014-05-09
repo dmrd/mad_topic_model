ce786c031bc73b79677da16ba1d4d2b48ddccb22
Charles Marsh <crmarsh@princeton.edu>
2014-05-08 21:33:41 -0400

    removed underscores in termite parser

--- visualization/parsers/termite.py
+++ visualization/parsers/termite.py
@@ -12,26 +12,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
     data = load(file(args.input_file).read())
 
-    for word_type in range(data['number_of_word_types']):
+    for word_type in range(data['number of word types']):
         dictionary = args.dictionaries[word_type]
         dictionary = file(dictionary).read().split('\n')
 
         results = data['metrics'][word_type]
 
         # Topic Index
-        topic_index = file('topic-index' + str(word_type) + '.txt', 'w+')
-        for i in range(results['number_of_topics']):
+        topic_index = file('topic index' + str(word_type) + '.txt', 'w+')
+        for i in range(results['number of topics']):
             topic_index.write(str(i) + '\n')
 
         # Term Index
         term_index = file('term-index' + str(word_type) + '.txt', 'w+')
-        for i in range(results['size_of_vocab']):
+        for i in range(results['size of vocab']):
             term_index.write(dictionary[i] + '\n')
 
         # Term-Topic Matrix
         term_topic = file('term-topic-matrix' + str(word_type) + '.txt', 'w+')
-        for i in range(results['number_of_topics']):
-            for j in range(results['size_of_vocab']):
-                n = results['vocab_distribution'][i][j]
+        for i in range(results['number of topics']):
+            for j in range(results['size of vocab']):
+                n = results['vocab distribution'][i][j]
                 term_topic.write(str(n) + ' ')
             term_topic.write('\n')
