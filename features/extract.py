1ee26c6d49bab7c9caebf5ace37b3eaa54b1c112
Charles Marsh <crmarsh@princeton.edu>
2014-05-10 12:54:42 -0400

    More extraction for ngram -> example

--- features/extract.py
+++ features/extract.py
@@ -6,7 +6,7 @@ def find_all_matching_ngrams(text, ngram, ngram_extractor, PUNC=True):
         return None
 
     n = len(ngram)
-    word_ngrams = analyzer.word_ngrams(text, n, PUNC=True)
+    word_ngrams = analyzer.word_ngrams(text, n, PUNC=PUNC)
     target_ngrams = ngram_extractor(text, n)
     return list(zip(*filter(lambda x: x[1] == ngram, zip(word_ngrams, target_ngrams)))[0])
 
@@ -16,4 +16,8 @@ def find_pos_ngram(text, ngram):
 
 
 def find_syllable_ngram(text, ngram):
-    return find_all_matching_ngrams(text, ngram, analyzer.syllable_ngrams, PUNC=False)
+    return find_all_matching_ngrams(text, ngram, analyzer.syllable_ngrams)
+
+
+def find_etymology_ngram(text, ngram):
+    return find_all_matching_ngrams(text, ngram, analyzer.etymology_ngrams, PUNC=False)
