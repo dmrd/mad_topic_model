d41b3883b71a6157f605d393a7f1db89879580b9
Shubhro Saha <shbhrsaha@gmail.com>
2014-05-11 01:01:14 -0400

    more thorough gutenberg and also ran word on 1-gram

---
+++ pipeline2.py
@@ -0,0 +1,23 @@
+"""
+Generates SLDA models for all the CSV files
+"""
+
+import os
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+CSV_FILENAMES = {"quora" : "data/quora.csv", "nweekly" : "data/nweekly.csv", "gutenberg" : "data/gutenberg.csv"}
+OUTPUT_FOLDER_NAME = "slda_input_files/"
+NGRAM_PARSERS = ['word']
+N_VALUES = [1]
+
+
+for filename in CSV_FILENAMES:
+    for N in N_VALUES:
+        for parser in NGRAM_PARSERS:
+            prefix = "%s%s_%s_%s" % (OUTPUT_FOLDER_NAME,filename,parser,N)
+            command = "python -um converter %s %s_model.txt %s_author.txt %s_ngram_dict.txt %s_author_dict.txt %s %s" % (CSV_FILENAMES[filename], prefix, prefix, prefix, prefix, parser, N)
+
+            logging.info("Executing: %s" % command)
+            os.system(command)
