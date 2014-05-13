e078783e2dfcf0223073a26afccba33de502808b
msimchowitz <msimchowitz@gmail.com>
2014-05-12 23:26:57 -0400

    reconstruct writes to file

--- slda_input_files/reconstruct.py
+++ slda_input_files/reconstruct.py
@@ -7,19 +7,22 @@ if __name__ == "__main__":
     parser.add_argument('term_file')
     args = parser.parse_args()
 
+    term_topic = file('tokens.txt', 'w+')
+
+
     terms = file(args.term_file).read().split('\n')
     i = 0
 
     for line in file(args.model_file).readlines():
         data = line.split(' ')[1:]
 
-        print str(i) + '\t',
+        term_topic.write( str(i) + '\t')
 
         for entry in data:
             entry = entry.split(':')
             term_index = int(entry[0])
             term = str(terms[term_index]).replace(' ', '')
             term_count = int(entry[1])
-            print ' '.join([term] * term_count),
-        print '\n',
+            term_topic.write( ' '.join([term] * term_count))
+        print term_topic.write('\n')
         i += 1
